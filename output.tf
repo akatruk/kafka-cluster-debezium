242db2623d8fc8119a2ece2ff9ec2c0e65b805ff
Andrei Katruk <a.katruk@live.com>
2021-10-20 15:07:26 +0300

    added any & checked

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-output "instance_ips" {
-  value = aws_instance.amazon[*].public_ip
-}
+# output "instance_ips" {
+#   value = aws_instance.amazon[*].public_ip
+# }
 
-output "instance_dns_names" {
-  value = aws_route53_record.kafka[*].name
-}
+# output "instance_dns_names" {
+#   value = aws_route53_record.kafka[*].name
+# }
